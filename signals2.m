3b474ac89ea257bb08ef36fbd2700562c71bd2b8
Roman58-326 <romanryckov58@gmail.com>
2026-03-06 11:23:10 +0600

    Update signals2.m

--- signals2.m
+++ signals2.m
@@ -6,25 +6,38 @@ points = 1:1:length(t_array);
 tones = 10; % Количество тонов
 f1 = input('Задайте нижнюю границу диапазона частот: ');
 f2 = input('Задайте верхнюю границу диапазона частот: ');
-df=(f2-f1)/tones;10 % Шаг частоты
-S = zeros(tones, length(t_array)); % Двумерный массив сигналов
-S_fft = zeros(tones, length(t_array));
-S_fft_shifted = zeros(tones, length(t_array));
-S_fft_mod = zeros(tones, length(t_array)); % Двумерный массив амплитуд спектров
-S_fft_ang = zeros(tones, length(t_array));
-S_fft_mod_db = zeros(tones, length(t_array)); % Двумерный массив магнитуд спектров (в децибелах)
+df=(f2-f1)/tones; % Шаг частоты
+S = zeros(1,length(t_array)); % Массив сигнала
+S_fft = zeros(1,length(t_array));
+S_fft_shifted = zeros(1,length(t_array));
+S_fft_mod = zeros(1,length(t_array)); % Массив амплитуд спектров
+S_fft_ang = zeros(1,length(t_array));
 for k = 1:tones
-   S(k, :) = A*exp(2*pi*t_array*1i*k*df);
-   S_fft(k, :) = fft(S(k, :));
-   S_fft_shifted(k, :) = fftshift(S_fft(k, :));
-   S_fft_mod(k, :) = abs(S_fft_shifted(k, :)/length(S_fft_shifted(k, :)));
-   S_fft_ang(k, :) = atan(S_fft_shifted(k, :));
-   S_fft_mod_db(k, :) = 20*log10(S_fft_mod(k, :)/A); % Перевод магнитуды спектра в децибелы
+   St = A*exp(2*pi*t_array*1i*k*df);
+   S = S + St;
+   St_fft = fft(St);
+   S_fft = S_fft + St_fft;
+   St_fft_shifted = fftshift(St_fft);
+   S_fft_shifted = S_fft_shifted + St_fft_shifted;
+   St_fft_mod = abs(St_fft_shifted/length(St_fft_shifted));
+   S_fft_mod = S_fft_mod + St_fft_mod;
+   St_fft_ang = atan(St_fft_shifted);
+   S_fft_ang = S_fft_ang + St_fft_ang;
+   k = k + 1;
   end;
- figure(1);
- plot(points, S_fft_mod(1,:), points, S_fft_mod(2,:), points, S_fft_mod(3,:), points, S_fft_mod(4,:), points, S_fft_mod(5,:), points, S_fft_mod(6,:), points, S_fft_mod(7,:), points, S_fft_mod(8,:), points, S_fft_mod(9,:), points, S_fft_mod(10,:));
- title('Спектр сигнала');
+  S_fft_mod_db = 20*log10(S_fft_mod/A); % Перевод магнитуды спектра в децибелы
+  figure(1);
+  plot(S);
 
- figure(2);
- plot(points, S_fft_mod_db(1,:), points, S_fft_mod_db(2,:), points, S_fft_mod_db(3,:), points, S_fft_mod_db(4,:), points, S_fft_mod_db(5,:), points, S_fft_mod_db(6,:), points, S_fft_mod_db(7,:), points, S_fft_mod_db(8,:), points, S_fft_mod_db(9,:), points, S_fft_mod_db(10,:));
- title('Амплитуда в дБ');
+  figure(2);
+  plot(S_fft);
+
+  figure(3);
+  plot(S_fft_mod);
+
+  figure(4);
+  plot(S_fft_mod_db);
+
+
+
+
